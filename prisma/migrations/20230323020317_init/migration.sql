c41ee71944777bec0e6432c01b229c702767a919
joshbrusa <joshbrusa@gmail.com>
2023-03-22 20:05:42 -0700

    add cron

---
+++ prisma/migrations/20230323020317_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Friend` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `fromId` INTEGER NOT NULL,
+    `toId` INTEGER NOT NULL,
+    `status` ENUM('pending', 'accepted') NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Ticker` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `ticker` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MarketDataFollow` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Friend` ADD CONSTRAINT `Friend_fromId_fkey` FOREIGN KEY (`fromId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Friend` ADD CONSTRAINT `Friend_toId_fkey` FOREIGN KEY (`toId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MarketDataFollow` ADD CONSTRAINT `MarketDataFollow_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/cron/tickerCron.ts
+++ src/cron/tickerCron.ts
@@ -1,7 +1,31 @@
 import { schedule } from "node-cron";
+import { prisma } from "../config/prismaClient";
 
 export default function tickerCron() {
-  schedule("* * * * 0-7", () => {
-    console.log("running a task every day of the week");
+  schedule("* * * * 0-7", async () => {
+    try {
+      const ploygonRes = await fetch(
+        "https://api.polygon.io/v3/reference/tickers?active=true&apiKey=" +
+          process.env.POLYGON_API_KEY
+      );
+
+      if (ploygonRes.ok) {
+        const data = await ploygonRes.json();
+
+        for (const val of data.results) {
+          await prisma.ticker.upsert({
+            create: {
+              id: val.ticker,
+            },
+            update: {},
+            where: { id: val.ticker },
+          });
+        }
+      } else {
+        console.log("ticker cron fetch failed");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   });
 }
